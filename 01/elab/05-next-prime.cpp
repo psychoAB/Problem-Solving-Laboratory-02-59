d6555ccd9a97119f1cf8f42a03b2ecbf3ee34c09
psychoAB <dome.2499@hotmail.com>
2017-01-20 14:35:37 +0700

    week01: complete 05-next-prime

--- 01/elab/05-next-prime.cpp
+++ 01/elab/05-next-prime.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
+using namespace std;
+
+bool is_prime(int number)
+{
+    if(number <= 1)
+    {
+        return false;
+    }
+    for(int i = 2; i < number; i++)
+    {
+        if(!(number % i))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(void)
 {
+    int x;
+    
+    cin >> x;
+    int i = x + 1;
+    while(!is_prime(i))
+    {
+        i++;
+    }
+    cout << i << endl;
 
     return 0;
 }
