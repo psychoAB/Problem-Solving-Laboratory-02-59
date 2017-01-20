b563bd08e1c63feb56c625fa69c9de5ecab4bedb
psychoAB <dome.2499@hotmail.com>
2017-01-20 14:47:28 +0700

    week01: complete 06-classic-stars

--- 01/elab/06-classic-stars.cpp
+++ 01/elab/06-classic-stars.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
+    int n;
+
+    cin >> n;
+
+    for(int line = 1; line <= n; line++)
+    {
+        for(int space = line; space < n; space++)
+        {
+            cout << " ";
+        }
+
+        for(int star = 0; star < line ; star++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
