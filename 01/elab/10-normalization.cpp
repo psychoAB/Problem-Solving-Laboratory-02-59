6406ca1cd4cd9426df7318c2d29b2f30e08901ac
psychoAB <dome.2499@hotmail.com>
2017-01-26 01:57:39 +0700

    week01: complete 10-normalization

--- 01/elab/10-normalization.cpp
+++ 01/elab/10-normalization.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main(void)
 {
+    int a,b,i;
+
+    cin >> a >> b;
+    if(a > b)
+    {
+        i = a;
+    }
+    else
+    {
+        i = b;
+    }
+
+    while(!(b % i == 0 && a % i == 0 || i < 1))
+    {
+        i--;
+    }
+    a /= i;
+    b /= i;
+    
+    cout << a << "/" << b << endl;
     return 0;
 }
