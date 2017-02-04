fa78ceb5731487a2759ebb6177607243ff03d52e
psychoAB <dome.2499@hotmail.com>
2017-02-05 00:36:35 +0700

    week1: completed robot1000.cpp

--- 01/grader/robot1000.cpp
+++ 01/grader/robot1000.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main(void)
 {
+    char command[101];
+    cin >> command;
+
+    char *ptr = command;
+    int x, y;
+    x = y = 0;
+    while(*ptr != '\0')
+    {
+        if(*ptr == 'N')
+        {
+            y += 1;
+        }
+        else if(*ptr == 'E')
+        {
+            x += 1;
+        }
+        else if(*ptr == 'S')
+        {
+            y -= 1;
+        }
+        else if(*ptr == 'W')
+        {
+            x -= 1;
+        }
+        else
+        {
+            x = 0;
+            y = 0;
+        }
+        ptr++;
+    }
+    cout << x << " " << y << endl;
 
     return 0;
 }
