026a414f87f51384dc39d3ddc9d386b662ee3f97
psychoAB <dome.2499@hotmail.com>
2017-02-05 01:48:09 +0700

    week1: completed robot2000.cpp

--- 01/grader/robot2000.cpp
+++ 01/grader/robot2000.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 int main(void)
 {
+    char command[101];
+    cin >> command;
+    char face = 'N';
+
+    char *ptr = command;
+    char r2k_cmd;
+    while(*ptr != '\0')
+    {
+        if(*ptr == 'Z')
+        {
+            face = 'N';
+            cout << 'Z';
+            ptr++;
+        }
+        else if(face != *ptr)
+        {
+            cout << 'R';
+            if(face == 'N')
+            {
+                face = 'E';
+            }
+            else if(face == 'E')
+            {
+                face = 'S';
+            }
+            else if(face == 'S')
+            {
+                face = 'W';
+            }
+            else
+            {
+                face = 'N';
+            }
+        }
+        else
+        {
+            cout << 'F';
+            ptr++;
+        }
+    }
+    cout << endl;
 
     return 0;
 }
