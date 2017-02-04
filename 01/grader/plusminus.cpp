003f12544854b4a15c3702b045f31193a49caf9f
psychoAB <dome.2499@hotmail.com>
2017-02-05 01:21:56 +0700

    week1: completed plusminus.cpp

--- 01/grader/plusminus.cpp
+++ 01/grader/plusminus.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main(void)
 {
+    int n;
+    cin >> n;
+    
+    int a, b;
+    char sign;
+    cin >> a;
+    for(int i = 0; i < n - 1; i++)
+    {
+        cin >> sign;
+        cin >> b;
+
+        if(sign == '+')
+        {
+            a += b;
+        }
+        else
+        {
+            a -= b;
+        }
+    }
+    cout << a << endl;
 
     return 0;
 }
