4f0cfaa04c27011f664e3506b4a44879f687c772
psychoAB <dome.2499@hotmail.com>
2017-02-13 22:41:21 +0700

    week2: completed cyclingchars.cpp

--- 02/cyclingchars.cpp
+++ 02/cyclingchars.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 int main(void)
 {
+    char c[101];
+    cin >> c;
+    
+    char *ptr;
+    ptr = c;
+    while(*ptr != '\0')
+    {
+        char x = *ptr;
+        for(int i = 0; i < 4; i++)
+        {
+            x++;
+            if(x > 'z')
+            {
+                x = 'a';
+            }
+        }
+        cout << x;
+        ptr++;
+    }
+    cout << endl;
+
     return 0;
 }
