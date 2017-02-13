9946b0741d8db1871cf6ed700a5a93982f7071e9
psychoAB <dome.2499@hotmail.com>
2017-02-13 22:26:31 +0700

    week1: completed 09-trucks.cpp

--- 01/elab/09-trucks.cpp
+++ 01/elab/09-trucks.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void)
 {
+    int n;
+    cin >> n;
+    vector<int> t;
+    
+    for(int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        int j = 0;
+        while(j < t.size())
+        {
+            if(t[j] + temp <= 1000)
+            {
+                t[j] += temp;
+                break;
+            }
+            j++;
+        }
+        if(j == t.size())
+        {
+            t.push_back(temp);
+        }
+    }
+    cout << t.size() << endl;
+
     return 0;
 }
