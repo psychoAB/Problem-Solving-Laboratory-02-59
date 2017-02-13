8d9debb5d9ff0bbaa40588c74f0564cfb3acad0e
psychoAB <dome.2499@hotmail.com>
2017-02-13 21:19:15 +0700

    week1: completed 08-close-enough.cpp

--- 01/elab/08-close-enough.cpp
+++ 01/elab/08-close-enough.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 int main(void)
 {
+    int n;
+    int l;
+    cin >> n >> l;
+    int h[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> h[i];
+    }
+
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            int dis = abs(h[i] - h[j]);
+            if(dis <= l)
+            {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
 
     return 0;
 }
