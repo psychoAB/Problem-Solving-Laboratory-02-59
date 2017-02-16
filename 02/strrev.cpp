60f63c321e72e7b3d5163885b6201c23d44a82fb
psychoAB <dome.2499@hotmail.com>
2017-02-16 22:46:26 +0700

    week2: completed strrev.cpp

--- 02/strrev.cpp
+++ 02/strrev.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main(void)
 {
+    char x[1001];
+    cin >> x;
+
+    char *ptr;
+    ptr = x;
+
+    while(*ptr != '\0')
+    {
+        ptr++;
+    }
+
+    ptr--;
+    while(ptr != x)
+    {
+        cout << *ptr;
+        ptr--;
+    }
+    cout << *ptr;
+    cout << endl;
+
     return 0;
 }
