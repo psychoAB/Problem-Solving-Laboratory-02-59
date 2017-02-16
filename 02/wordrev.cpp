bbd77017b99823a5b1e26a158669481c1e4465ca
psychoAB <dome.2499@hotmail.com>
2017-02-16 23:08:47 +0700

    week2: completed wordrev.cpp

--- 02/wordrev.cpp
+++ 02/wordrev.cpp
@@ -2,7 +2,38 @@
 
 using namespace std;
 
+void r(const char *head, char *tail)
+{
+    while(tail != head)
+    {
+        cout << *tail;
+        tail--;
+    }
+    cout << *tail;
+}
+
 int main(void)
 {
+    char x[1001];
+    cin >> x;
+
+    char *head, *cur;
+    
+    head = x;
+    cur = head;
+
+    while(*cur != '\0')
+    {
+        if(*cur == '_')
+        {
+            r(head, cur - 1);
+            cout << *cur;
+            head = cur + 1;
+        }
+        cur++;
+    }
+    r(head, cur - 1);
+    cout << endl;
+
     return 0;
 }
