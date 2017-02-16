77800b834887bd858438e6540f4164275ae34fe7
psychoAB <dome.2499@hotmail.com>
2017-02-16 16:52:43 +0700

    week2: completed upperall.cpp

--- 02/upperall.cpp
+++ 02/upperall.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main(void)
 {
+    char text[101];
+    char *ptr;
+
+    cin >> text;
+
+    ptr = text;
+    while(*ptr != '\0')
+    {
+        if(*ptr >= 'a' && *ptr <= 'z')
+        {
+            cout << (char)(*ptr - 32);
+        }
+        else
+        {
+            cout << *ptr;
+        }
+        ptr++;
+    }
+
     return 0;
 }
