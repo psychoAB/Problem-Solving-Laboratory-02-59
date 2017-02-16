38824525208a2476ef2ffd87beec52281b66d4a7
psychoAB <dome.2499@hotmail.com>
2017-02-16 23:21:23 +0700

    week2: completed sentrev.cpp

--- 02/sentrev.cpp
+++ 02/sentrev.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+void p(char *ptr, const char *t)
+{
+    while(ptr != t)
+    {
+        cout << *ptr;
+        ptr++;
+    }
+    cout << *ptr;
+}
+
 int main(void)
 {
+    char x[1001];
+    cin >> x;
+    
+    char *ptr, *t;
+    ptr = x;
+
+    while(*ptr != '\0')
+    {
+        ptr++;
+    }
+    ptr--;
+    t = ptr;
+
+    while(ptr != x)
+    {
+        if(*ptr == '_')
+        {
+            p(ptr + 1, t);
+            cout << *ptr;
+            t = ptr - 1;
+        }
+        ptr--;
+    }
+    p(ptr, t);
+    cout << endl;
+
     return 0;
 }
