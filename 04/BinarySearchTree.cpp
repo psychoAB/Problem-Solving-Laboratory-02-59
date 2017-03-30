a228365abf9e28a28b8d1afa7890904f6f449706
psychoAB <dome.2499@hotmail.com>
2017-03-30 14:17:27 +0700

    week04: update insert() and find() in BinarySearchTree.cpp

--- 04/BinarySearchTree.cpp
+++ 04/BinarySearchTree.cpp
@@ -7,3 +7,40 @@ struct TreeNode
     TreeNode(int val, TreeNode *left=0, TreeNode *right=0)
         : val(val), left(left), right(right) {}
 }
+
+void insert(TreeNode *r, int x)
+{
+    if(!r)
+    {
+        r = new TreeNode(x);
+    }
+    else if(x < r -> val)
+    {
+        insert(r->left, x);
+    }
+    else if(x > r -> val)
+    {
+        insert(r->right, x);
+    }
+}
+
+int find(TreeNode *r, int x)
+{
+    if(!r)
+    {
+        return 0;
+    }
+    else if(x == r-> val)
+    {
+        return 1;
+    }
+    else if(x < r -> val)
+    {
+        return find(r->left, x);
+    }
+    else if(x > r -> val)
+    {
+        return find(r->right, x);
+    }
+    return 0;
+}
