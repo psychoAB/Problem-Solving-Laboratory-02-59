78864f8dfced3ff07b70937d92cec731d99c4933
psychoAB <dome.2499@hotmail.com>
2017-03-30 14:02:33 +0700

    week04: init week 04

---
+++ 04/BinarySearchTree.cpp
@@ -0,0 +1,9 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val, TreeNode *left=0, TreeNode *right=0)
+        : val(val), left(left), right(right) {}
+}
