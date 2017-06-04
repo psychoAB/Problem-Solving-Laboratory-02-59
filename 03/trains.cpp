1c9467df36244080570a215e1250d461d5f7c8d8
psychoAB <dome.2499@hotmail.com>
2017-06-04 08:09:42 +0700

    week03: trains.cpp first sent [T]

---
+++ 03/trains.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode(int val, ListNode *next = 0)
+        : val(val), next(next) {}
+};
+
+void n(ListNode *head, int v)
+{
+    ListNode *c;
+    c = head;
+    while(c->next != NULL)
+    {
+        c = c->next;
+    }
+    c->next = new ListNode(v);
+}
+
+void mo(ListNode *a, ListNode *b)
+{
+    ListNode *c;
+    c = b;
+    while(c->next != NULL)
+    {
+        c = c->next;
+    }
+    c->next = a->next;
+    a->next = NULL;
+}
+
+int main()
+{
+    ListNode *head[100000];
+    for(int i = 0; i < 100000; i++)
+    {
+        head[i] = new ListNode(0);
+    }
+    int m;
+    scanf("%d", &m);
+    
+    for(int i = 0; i < m; i++)
+    {
+        char c;
+        int a, b;
+        cin >> c >> a >> b;
+        
+        if(c == 'N')
+        {
+            n(head[b - 1], a);
+        }
+        else if(c == 'M')
+        {
+            mo(head[a - 1], head[b - 1]);
+        }
+    }
+
+    for(int i = 0; i < 100000; i++)
+    {
+        ListNode *x = head[i];
+        while(x != NULL)
+        {
+            if(x != head[i])
+            {
+                printf("%d\n", x->val);
+            }
+            x = x->next;
+        }
+    }
+    return 0;
+}
